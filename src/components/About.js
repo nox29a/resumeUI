2ae503014b4a08a0ff57ca0af7f1e002e4d39928
nox29a <adamczyk29a@interia.pl>
2023-03-27 15:43:38 +0200

    updated version

--- src/components/About.js
+++ src/components/About.js
@@ -7,8 +7,8 @@ const About = () => {
       <div className='container mx-auto'>
         <h1 className='text-[90px] text-center p-8 gap-4'>About me</h1>
             <div className='text-lg grid grid-cols-1 md:grid-cols-2  p-8 gap-20 '>
-              <p className='bg-hero border-4 border-purple-700 text-xl rounded-lg p-8 max-w-[500px] items-center '>I'm excited to be starting my journey in web development. While I may not have any professional experience yet, I am passionate about learning and developing my skills in this field.</p>
-              <p className='bg-hero border-4 border-purple-700 text-xl  rounded-lg p-8 max-w-[500px]'>I recently completed a FreeCodeCamp Course, where I earned some certifications. Since then, I've been building my own projects, experimenting with different technologies, and staying up-to-date with the latest trends in web development.</p>
+              <p className='bg-hero border-4 border-purple-700 text-xl rounded-lg p-8 '>I'm excited to be starting my journey in web development. While I may not have any professional experience yet, I am passionate about learning and developing my skills in this field.</p>
+              <p className='bg-hero border-4 border-purple-700 text-xl  rounded-lg p-8 '>I recently completed a FreeCodeCamp Course, where I earned some certifications. Since then, I've been building my own projects, experimenting with different technologies, and staying up-to-date with the latest trends in web development.</p>
             </div>
       </div>
     </div>
--- src/components/Skills.js
+++ src/components/Skills.js
@@ -12,7 +12,7 @@ const Skills = () => {
   <div id='skill' className='grid place-items-center bg-hero pt-32'>
     <div className='container mx-auto'>
       <h1 className='text-[90px] text-center p-8 gap-4'>Skills</h1>
-        <div className='text-lg grid grid-cols-3 md:grid-cols-4  p-8 gap-20 '>
+        <div className='text-lg grid grid-cols-3 md:grid-cols-4  p-8 gap-20 mx-40'>
           {icons.map((icon, index) => (<img src={icon} alt='icon' className='max-w-[70px] max-h-[70px] animate-wiggle hover:animate-spin '></img>))}
         </div>
     </div>
