172f1f728995eca9351602791ec212a277544fb7
nox29a <adamczyk29a@interia.pl>
2023-09-14 18:08:13 +0200

    upgraded hero

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -15,7 +15,7 @@ const Hero = () => {
       </span></h1>
     </div>
     <div className='z-1 animate-in md:flex-rows flex justify-end    mt-96 lg:mt-20'>
-        <img className=' flex max-w-[400px] max-h-[400px] align-bottom rounded-full border-4 border-purple-900' src={HeroImg} alt="hero"></img>
+        <img className=' flex max-w-[200px] max-h-[200px] lg:max-w-[400px] lg:max-h-[400px] align-bottom rounded-full border-4 border-purple-900' src={HeroImg} alt="hero"></img>
       </div>
   </div>
 </div>
